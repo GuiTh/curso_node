5a31983193a91d97a32e512e8a23f5ecf35ed120
GuiTh <guilhermetcarlos@hotmail.com>
2023-02-14 17:43:40 -0300

    att

--- projeto_guiaPress/articles/articlesController.js
+++ projeto_guiaPress/articles/articlesController.js
@@ -12,6 +12,7 @@ router.get('/admin/articles', (req,res)=>{
 
     })
 });
+
 router.get('/admin/articles/new', (req,res) =>{
     Category.findAll().then(categories =>{
         res.render('admin/articles/new', {categories:categories})
@@ -42,14 +43,86 @@ router.post('/articles/delete', (req,res)=>{
                     id:id
                 }
             }).then(() =>{
-                res.redirect('/admin/categories')
+                res.redirect('/admin/articles')
             })
         }else{
-            res.redirect('/admin/categories')
+            res.redirect('/admin/articles')
         }
     }else{
-        res.redirect('/admin/categories')
+        res.redirect('/admin/articles')
     }
 })
+
+router.get('/admin/articles/edit/:id', (req,res) =>{
+    var id = req.params.id
+    if(isNaN(id)){
+        res.redirect('admin/articles')
+    }
+    Article.findByPk(id).then(article =>{
+        if(article != undefined){
+            Category.findAll().then(categories =>{
+                res.render('admin/articles/edit', {categories: categories, article:article})
+            })
+        }else{
+            res.redirect('/admin/articles')
+        }
+    }).catch(err =>{
+        res.redirect('/admin/articles', {err})
+    })
+})
+
+router.post('/articles/update',(req,res) =>{
+
+    var id = req.body.id
+    var title = req.body.title
+    var body = req.body.body
+    var category =req.body.category
+    
+    Article.update({title: title, body: body, categoryId:category, slug: slugify(title)}, {
+        where:{
+            id:id
+        }
+    }).then(()=>{
+        res.redirect('/admin/articles')
+    })
+})
+
+router.get('/articles/page/:num', (req,res)=>{
+    var page = req.params.num
+    var offset = 0
+
+    if(isNaN(page) || page == 1){
+        offset = 0
+    }else{
+        offset = (parseInt(page) -1) * 4
+    }
+    Article.findAndCountAll({
+        limit : 4,
+        offset: offset,
+        order:[
+            ['id', 'DESC']
+        ],
+    }).then(articles =>{
+
+        var next
+        if(offset * 4 >= articles.count){
+            next = false
+        }else{
+            next = true
+        }
+
+        var result = {
+            page: parseInt(page),
+            next: next,
+            articles: articles,
+        }
+
+        Category.findAll().then(categories =>{
+            res.render('admin/articles/page', {result:result, categories:categories, page: page})
+        })
+    })
+})
+
+
 
 module.exports = router;
--- projeto_guiaPress/index.js
+++ projeto_guiaPress/index.js
@@ -5,6 +5,7 @@ const connection = require('./database/database')
 
 const categoriesController = require('./categories/CategoriesController')
 const articlesController = require('./articles/articlesController')
+const usersController = require('./user/userController')
 
 const Article = require('./articles/articles')
 const Category = require('./categories/category')
@@ -31,10 +32,18 @@ connection.authenticate()
 
 app.use('/', categoriesController)
 app.use('/', articlesController)
+app.use('/', usersController)
 
 app.get('/', (req,res)=>{
-    Article.findAll().then(articles =>{
-        res.render('index', {articles})
+    Article.findAll({
+        order:[
+            ['id', 'DESC']
+        ],
+        limit: 4
+    }).then(articles =>{
+        Category.findAll().then(categories =>{
+            res.render('index', {articles, categories})
+        })
     })
 })
 
@@ -47,6 +56,27 @@ app.get('/:slug', (req,res) =>{
     }).then(article =>{
         if(article != undefined){
             res.render('article', {article:article});
+        }else{
+            res.redirect('/')
+        }
+    }).catch(err =>{
+        res.redirect('/')
+    })
+})
+
+app.get('/category/:slug', (req,res) =>{
+    var slug = req.params.slug
+
+    Category.findOne({
+        where:{
+            slug:slug
+        },
+        include:[{model: Article}]
+    }).then(category =>{
+        if(category != undefined){
+            Category.findAll().then(categories =>{
+                res.render('index',{articles: category.articles, categories: categories})
+            })
         }else{
             res.redirect('/')
         }
