755e0074e80aa488711b0a62145a5e7c7a47ec8c
GuiTh <guilhermetcarlos@hotmail.com>
2023-02-14 17:56:33 -0300

    att

--- projeto_guiaPress/user/userController.js
+++ projeto_guiaPress/user/userController.js
@@ -10,4 +10,21 @@ router.get('/admin/users/create', (req,res)=>{
     res.render('admin/users/create')
 })
 
+router.get('/admin/users', (req,res)=>{
+    User.findAll().then((users) =>{
+        res.render('admin/users/users',{users:users})
+    })
+})
+
+router.post('/admin/users/new', (req,res)=>{
+    var email = req.body.email
+    var password = req.body.password
+    User.create({
+        email:email,
+        password:password
+    }).then(()=>{
+        res.redirect('/admin/users')
+    })
+})
+
 module.exports = router
